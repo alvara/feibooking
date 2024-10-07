53f0b0443d24929b6af19a23784a1d2db187cc34
Jesse Alvarado <contact@jessealvarado.com>
2024-10-07 13:09:09 +0900

    refactor: remove head text

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -2,38 +2,31 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
-    const metaUrl = 'https://www.reboot-japan.com/';
-    const metaDescription =
-      'あこがれの最新デバイスをお値頃価格で。リーズナブルな価格と、自由な契約期間でサブスクできます。最新デバイスを気軽にサブスクして、デジタル・ライフを"リブート"しましょう！	';
+    // const metaUrl = 'https://www.reboot-japan.com/';
+    // const metaDescription = 'Automated booking system for Fei Fei';
 
     return (
       <Html lang="en">
         {/* Head for global on all pages (not to be confused with PageHead for individual page changes) */}
         <Head>
-          {/* PWA primary color */}
+          {/* 
           <meta name="theme-color" content="" />
           <meta name="description" content={metaDescription} />
 
-          {/* favicons */}
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
 
-          {/* Open Graph / Facebook */}
           <meta property="og:type" content="website" />
-          <meta property="og:url" content={`${metaUrl}/`} />
           <meta property="og:title" content={metaDescription} />
           <meta property="og:description" content={metaDescription} />
-          <meta property="og:image" content={`${metaUrl}/ogp.png`} />
 
-          {/*Twitter*/}
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={`${metaUrl}/`} />
           <meta property="twitter:title" content={metaDescription} />
           <meta property="twitter:description" content={metaDescription} />
-          <meta property="twitter:image" content={`${metaUrl}/ogp.png`} />
+          */}
         </Head>
         <body>
           <Main />
